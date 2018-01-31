ee6e8a5ab1e66ef92a57a634659f69dfbcb085ea
afk7 <andresfelipe5642@hotmail.com>
2018-01-31 16:01:35 -0500

    Implementacion de los checkbox dinamicos en el menu emocional

---
+++ library/checkbox dinamicos/checkbox_a_emocional.php
@@ -0,0 +1,12 @@
+<?php
+	$consulta="SELECT nombre_emocional FROM reporte_emocional";
+	$resultado = $conexion->query($consulta);
+	$count = $resultado->num_rows;
+
+	if($count >= 1) {
+		while($row=mysqli_fetch_array($resultado)){
+			echo "<input type='checkbox' name='alteraciones_emocionales[]' id='$row[nombre_emocional]' id='$row[nombre_emocional]' value='$row[nombre_emocional]'>";
+			echo "<label for='$row[nombre_emocional]'>$row[nombre_emocional]</label>";
+		}
+	}
+?>
