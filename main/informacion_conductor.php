b80a90f1c1db762966c37f27502a889ed17c7cd9
afk7 <andresfelipe5642@hotmail.com>
2018-04-29 20:01:06 -0500

    Reorganizacion de los elementos en el panel de informacion conductor

--- main/informacion_conductor.php
+++ main/informacion_conductor.php
@@ -1,7 +1,7 @@
 <?php
 	echo "<div class='container'>
 			<div class='panel panel-primary contenedor_info_conductor text-center'>
-					<div class='panel-heading'><span class='fa fa-file-text fa-2x'></span> <br>Informacion del Conductor</div>
+					<div class='panel-heading'><span class='fa fa-file-text fa-2x'></span> <br><h3>Informacion del Conductor</h3></div>
 					<div class='panel-body'>
 						<div class='col-md-12'>
 							<img class='img-thumbnail img-rounded' width='200'  height='200' src='$_SESSION[foto]'><br><br><hr class='without-style-hr'>
