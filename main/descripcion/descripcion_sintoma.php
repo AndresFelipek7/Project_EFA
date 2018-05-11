6eb974aff2d8a7e90de6d25e9aa93816849c2ff1
afk7 <andresfelipe5642@hotmail.com>
2018-05-10 21:10:10 -0500

    Refactorizacion del modulo de sintomas en el render de sugerencias con el switch mejorado y dinamico

--- main/descripcion/descripcion_sintoma.php
+++ main/descripcion/descripcion_sintoma.php
@@ -5,7 +5,7 @@
 		<div class='modal-content'>
 			<div class='modal-header'>
 				<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-				<h4 class='modal-title'><span class="glyphicon glyphicon-heart"></span> <br> Descripcion Detallada</h4>
+				<h4 class='modal-title'><span class="glyphicon glyphicon-heart fa-2x"></span> <br> Descripcion Detallada</h4>
 			</div>
 			<div class='modal-body text-center'>
 				<?php
