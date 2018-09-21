9b11949068281064e872b8f29ea1c9ca04ae9a80
afk7 <andresfelipe5642@hotmail.com>
2018-09-20 21:51:39 -0500

    Implementacion de la funcion de precision de la captura de la informacion de pulsaciones del conductor(Parcial).

--- Evaluacion.php
+++ Evaluacion.php
@@ -115,13 +115,23 @@
 									<hr>
 								</div>
 								<div class="col-md-12">
-									<button type="button" class="btn btn-md btn-primary" id="ayuda_pulsaciones" data-toggle="tooltip" data-placement="left" title="Las pulsaciones es el pulso que presente el conductor durante la evaluacion de fatiga , si por alguna razon las pulsaciones superan las 100 por minuto es necesario repetir la medicion de las pulsaciones para estar seguros. solo se coloca el numero entero en este campo."><span class="fa fa-info"></span></button>
+									<button type="button" class="btn btn-md btn-primary" id="ayuda_pulsaciones" data-toggle="tooltip" data-placement="left" title="Las pulsaciones es el pulso que presente el conductor durante la evaluacion de fatiga , si por alguna razon las pulsaciones superan las 100 por minuto es necesario repetir la medicion de las pulsaciones para estar seguros. solo se coloca el numero entero en este campo."><span class="fa fa-info"></span></button><br>
+									<label>Valor Pulsaciones </label>
 									<center>
-										<label>Valor Pulsaciones </label>
-										<input type="number" name="pulsaciones" class="form-control" id="pulsaciones" placeholder="Ingrese valor numerico" onkeypress="return justNumbers(event,this.form.desde.value);" onchange="limit_pulsation_hearth(); style_border_input('pulsaciones','verde')" required>
+										<div class="row">
+											<div class="col-md-6">
+												<input type="number" name="pulsaciones_p1" class="form-control" id="pulsaciones_p1" placeholder="Primer Valor" onkeypress="return justNumbers(event,this.form.desde.value);" onchange="limit_pulsation_hearth(); style_border_input('pulsaciones_p1','verde')" required>
+											</div>
+											<div class="col-md-6">
+												<input type="number" name="pulsaciones_p2" class="form-control" id="pulsaciones_p2" placeholder="Segundo Valor" onkeypress="return justNumbers(event,this.form.desde.value);" onchange="limit_pulsation_hearth(); style_border_input('pulsaciones_p2','verde')" required>
+											</div>
+										</div>
 									</center>
 									<hr>
 								</div>
+
+								<div id="container_all_pulsaciones"></div>
+
 								<div class="col-md-12">
 									<a href="#usuario_contraseña_conductor" data-toggle='modal' class="btn btn-warning"><span class="glyphicon glyphicon-user"></span></a>
 									<button type="button" class="btn btn-md btn-primary" id="ayuda_sueño_profundo" data-toggle="tooltip" data-placement="right" title="Sueño profundo consiste en el tiempo en que el conductor a dormido."><span class="fa fa-info"></span></button>
--- register_test.php
+++ register_test.php
@@ -6,27 +6,6 @@
 	/*$insercion_Evaluacion = "INSERT INTO evaluacion_fatiga (id_usuario , id_conductor , id_ruta , ids_sintomas , id_signo , ids_nivel_riesgo , ids_sugerencia_sintomas , id_sugerencia_signo , ids_sugerencia_emocionales , id_orden , nivel_fatiga , origen , horas_descanso , horas_camarote , horas_conduciendo , horas_otra_actividad , cual_actividad , tiempo_sueno , tiempo_descanso , info_copiloto , origen_copiloto , tiempo_sueno_manilla , verdad_conductor_descanso , pulsaciones , descargable_fit , ids_a_emocional , sugerencia_horas_conducidas , cual_otro_sintoma , cual_otro_emocional , cual_otro_estado_signo , sintoma_hormigueo , tiempo_sueno_ligero) VALUES ( '$id_Usuario' , '$identificacion_conductor' , '$traer_Valores_interrogatorio[7]' , '$sintomas_seleccionados' , '$traer_Valores_signos' , '$pilares_seleccionados' , '$sugerencia_sintoma_seleccionados' , '$sugerencia_signo_seleccionado' , '$sugerencia_a_emocional_seleccionados' , '$valor_id_orden' , '$saber_nivel_fatiga' ,'Bogota' , '$traer_Valores_interrogatorio[0]' , '$traer_Valores_interrogatorio[1]' , '$traer_Valores_interrogatorio[2]' , '$traer_Valores_interrogatorio[3]' , '$traer_Valores_interrogatorio[4]' , '$sacar_Minutos_hora' , '$traer_Valores_interrogatorio[6]' , '$traer_Valores_interrogatorio[8]' , '$traer_Valores_interrogatorio[9]' , '$valor_Total_minutos_Manilla' , '$valor_verdad_tiempo_descanso_conductor' , '$traer_Valores_interrogatorio[10]' , '$path_rename_fit_image' , '$a_emocionales_seleccionadas' , '$recomendacion_Descansar_conducir' , '$otro_sintoma' , '$otra_a_emocional' , '$valor_otro_estado_signo' ,'$s_hromigueo' , '$sueno_ligero')";
 	$resultado_Insercion_evaluacion = $conexion -> query($insercion_Evaluacion);
 */
-
-	/*Notice: Undefined index: sueño_efectivo_previo in C:\wamp64\www\Project - EFA\variables.php on line 42
-
-	Notice: Undefined variable: tiempo_sueño_conductor in C:\wamp64\www\Project - EFA\register_test.php on line 63
-
-	Notice: Undefined index: tiempo_destino in C:\wamp64\www\Project - EFA\main\hallar_pilar.php on line 48
-
-	*/
-
-	/*$traer_Valores_interrogatorio = [
-		$tiempo_alistamiento ,
-		$tiempo_camarote ,
-		$tiempo_conduciendo ,
-		$tiempo_o_actividad ,
-		$_POST['cual_actividad'] ,
-		$tiempo_extralaboral ,
-		$_POST['ruta'] ,
-		$_POST['copiloto'] ,
-		$_POST['origen_copiloto'] ,
-		$_POST["pulsaciones"]];*/
-
 	$insercion_Evaluacion = "INSERT INTO evaluacion_fatiga (
 		id_usuario ,
 		id_conductor ,
